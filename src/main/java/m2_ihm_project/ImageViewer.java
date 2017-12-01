2762ee53b7045c02e13e12813b0ff13daf34bc13
JeanBrice <jeanbrice.canihac@gmail.com>
2017-12-01 22:05:29 +0100

    Adding img folder

--- src/main/java/m2_ihm_project/ImageViewer.java
+++ src/main/java/m2_ihm_project/ImageViewer.java
@@ -1,10 +1,22 @@
 package m2_ihm_project;
 
-public class ImageViewer {
+import javax.swing.JFrame;
+
+public class ImageViewer extends JFrame{
+
+	private int state;
+	
+	
+	
+	
+	public ImageViewer() {
+		super("Image Viewer");
+		this.state = 0;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("prout");
+		
 	}
 
 }
