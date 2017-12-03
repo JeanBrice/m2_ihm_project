8d6148fa805c12830be0d5d4ef82fdc65bfb4f21
jeanb <jeanb@PC-Rimey.home>
2017-12-03 12:57:18 +0100

    Complete project !!

--- src/main/java/m2_ihm_project/ImageViewer.java
+++ src/main/java/m2_ihm_project/ImageViewer.java
@@ -1,6 +1,7 @@
 package m2_ihm_project;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -14,12 +15,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.border.Border;
 
 public class ImageViewer extends JFrame{
 
@@ -33,12 +36,13 @@ public class ImageViewer extends JFrame{
 	JButton backward, play, stop, forward;
 	private Timer t;	
 	
-	private List<ImageIcon> imgs;
+	private List<JLabel> imgs;
 	private List<JPanel> wide_imgs;
 	
 	// Constructor
 	public ImageViewer() {
 		super("Image Viewer");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.state = 0;
 		this.setSize(600, 650);
 		this.setLayout(new BorderLayout());
@@ -69,6 +73,21 @@ public class ImageViewer extends JFrame{
 		this.btns_pane.add(forward);
 		
 		// Btns listener
+		this.stop.addMouseListener(new MouseListener() {
+			
+			public void mouseReleased(MouseEvent e) {}			
+			public void mousePressed(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			
+			public void mouseClicked(MouseEvent e) {
+				t.stop();
+				setState(0);
+				behave();
+			}
+		});
+		
+		
 		this.play.addMouseListener(new MouseListener() {
 			
 			public void mouseReleased(MouseEvent e) {}			
@@ -77,8 +96,38 @@ public class ImageViewer extends JFrame{
 			public void mouseEntered(MouseEvent e) {}
 			
 			public void mouseClicked(MouseEvent e) {
-				setState(2);
-				behave();
+				if(play.getText().equals("Start")) {
+					setState(2);
+					behave();
+				} else {
+					setState(1);
+					t.stop();
+					behave();
+				}
+			}
+		});
+		
+		this.forward.addMouseListener(new MouseListener() {
+			
+			public void mouseReleased(MouseEvent e) {}			
+			public void mousePressed(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			
+			public void mouseClicked(MouseEvent e) {
+				speedUp();
+			}
+		});
+		
+		this.backward.addMouseListener(new MouseListener() {
+			
+			public void mouseReleased(MouseEvent e) {}			
+			public void mousePressed(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			
+			public void mouseClicked(MouseEvent e) {
+				speedDown();
 			}
 		});
 		
@@ -95,18 +144,38 @@ public class ImageViewer extends JFrame{
 		
 	}
 	
+	public void speedUp() {
+		if(this.timerLevel < 3 && this.forward.isEnabled()) {
+			if(this.timerLevel == -1) {
+				this.timerLevel = 1;
+			} else {
+				this.timerLevel += 1;
+			}
+		}
+	}
+	
+	public void speedDown() {
+		if(this.timerLevel > -3 && this.backward.isEnabled()) {
+			if(this.timerLevel == 1) {
+				this.timerLevel = -1;
+			} else {
+				this.timerLevel -= 1;
+			}
+		}
+	}
+	
 	public void setState(int i) {
 		this.state = i;
 	}
 	
 	public void initImg() {
 		// Création des 9 Panels pour les images
-		this.imgs = new ArrayList<ImageIcon>();
+		this.imgs = new ArrayList<JLabel>();
 		for(int i = 1; i < 10; i++) {
 			try {
 				BufferedImage img = ImageIO.read(new File("img/img_"+i+".jpg"));
 				ImageIcon image = new ImageIcon(new ImageIcon(img).getImage().getScaledInstance(180, 180, Image.SCALE_DEFAULT));
-				this.imgs.add(image);
+				this.imgs.add(new JLabel(image));
 			} catch (IOException e) {
 				System.err.println("Unable to load image");
 			}
@@ -128,9 +197,26 @@ public class ImageViewer extends JFrame{
 	public void showList() {
 		//this.initImg();
 		this.imgs_pane = new JPanel(new GridLayout(3, 3));
-		for(ImageIcon ii: imgs) {
-			this.imgs_pane.add(new JLabel(ii));
+		for(final JLabel lab: imgs) {
+			lab.addMouseListener(new MouseListener() {				
+				public void mouseReleased(MouseEvent e) {}				
+				public void mousePressed(MouseEvent e) {}				
+				public void mouseExited(MouseEvent e) {}
+				public void mouseEntered(MouseEvent e) {}
+				
+				public void mouseClicked(MouseEvent e) {
+					for(JLabel l: imgs) {
+						l.setBorder(null);
+					}
+					Border bor = BorderFactory.createLineBorder(Color.BLUE,3);
+					lab.setBorder(bor);
+					current = imgs.indexOf(lab)+1;
+				}
+			});
+			this.imgs_pane.add(lab);
 		}
+		this.add(this.imgs_pane, BorderLayout.CENTER);
+		this.revalidate();
 	}
 	
 	// Mode visionneuse
@@ -177,31 +263,66 @@ public class ImageViewer extends JFrame{
 		}
 	}
 	
+	public void getNext(){
+		if(this.timerLevel > 0) {
+			if(this.current < 9)
+				this.current += 1;
+			else
+				this.current = 1;
+		} else {
+			if(this.current > 1)
+				this.current -= 1;
+			else
+				this.current = 9;
+		}
+	}
+	
 	// Comportement de l'application selon les états
 	public void behave() {
 		System.out.println("State: " + this.state);
 		switch(this.state) {
 		case 0:
+			t.stop();
 			current = 1;
 			showList();
 			backward.setEnabled(false);
 			forward.setEnabled(false);
 			stop.setEnabled(false);
+			play.setText("Start");
+			play.setToolTipText("Start slideshow");
 			break;
 			
 		case 1:
+			t.stop();
+			backward.setEnabled(false);
+			forward.setEnabled(false);
+			play.setText("Start");
+			play.setToolTipText("Resume slideshow");
 			break;
 			
 		case 2:
-			backward.setEnabled(true);
 			play.setText("Pause");
+			play.setToolTipText("Pause slideshow");
 			stop.setEnabled(true);
-			forward.setEnabled(true);
+			stop.setToolTipText("Stop slideshow");
+			if(this.timerLevel > 2) {
+				System.out.println("max. positive speed reached");
+				forward.setEnabled(false);
+			}
+			if(this.timerLevel > -3) {
+				backward.setEnabled(true);
+				backward.setToolTipText("Slow down or reverse scrolling direction");
+			}
+			if(this.timerLevel < -2) {
+				System.out.println("max. negative speed reached");
+				backward.setEnabled(false);
+			}
+			if(this.timerLevel < 3) {
+				forward.setEnabled(true);
+				forward.setToolTipText("Speed up");
+			}
 			showImg();
-			if(this.current < 9)
-				this.current += 1;
-			else
-				this.current = 1;
+			getNext();
 			justWait(getTimeToWait());
 			break;
 			
